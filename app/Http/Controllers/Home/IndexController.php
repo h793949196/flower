ff86b70dbca26ab5fd1a5d1f69a424110bf2ac30
h793949196 <793949196@qq.com>
2017-03-03 14:54:34 +0800

    Update IndexController.php

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers\Home;
 
-use App\Http\Model\Article;
-use App\Http\Model\Category;
-use App\Http\Model\Links;
-
 class IndexController extends CommonController
 {
     public function index()
